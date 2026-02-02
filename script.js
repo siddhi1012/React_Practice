ed489f03a6493492063e53e1c04c228780bc3d3e
Siddhi Gavhane <siddhigavhane03@gmail.com>
2026-02-02 11:24:33 +0530

    via Upload

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
 // Sum of 1 to 5
-let sum = 0
-for (let i = 1; i <= 5; i++) {
-    sum = sum + i;
+// let sum = 0
+// for (let i = 1; i <= 100; i++) {
+//     console.log("i:", i);
+// }
+// console.log("sum:", sum)
+
+// let i = 1 ;
+// while(i<=5){
+//     console.log("i:",i);
+//     i++;
+// }
+
+
+// let i = 1
+// do {
+//     console.log("i:", i)
+//     i++; 
+// } while (i <= 50)
+
+// let str = "Siddhi"
+// let size = 0;
+// for (let val of str) {
+//     console.log(val)
+//     size++;
+
+// }
+// console.log("String Size:", size);
+
+let student = {
+    name: "Siddhi Gavhane",
+    age: 20,
+    cgpa: 9.28,
+    isPass: true
 }
-console.log("sum:", sum);
+
+for (let key in student) {
+    console.log("key:", key, "|", "value:", student[key]);
+}
