b1b72447b3b754e606a94cb0d4946c20c65790ea
Siddhi Gavhane <siddhigavhane03@gmail.com>
2026-02-05 11:59:16 +0530

    via upload

--- react_app/src/component/Basics/Resturant.js
+++ react_app/src/component/Basics/Resturant.js
@@ -3,6 +3,7 @@ import "./style.css"
 import Menu from './menuApi'
 import MenuCard from './MenuCard'
 import Navbar from './Navbar'
+import useState from '../Hooks/useState'
 
 const uniqueList = [...new Set(Menu.map((curElem) => {
     return curElem.category;
---
+++ react_app/src/component/Hooks/useState.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import "./style.css"
+
+const useState = () => {
+    return (
+        <>
+            <div className="center_div">
+                <p>my data</p>
+                <div class="button2">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    HOVER ME
+                </div>
+                <div class="button2">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    HOVER ME
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default useState
